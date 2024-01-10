f316f68776143e64cccd29260878762e50f57a7c
Adnan <adnanvandyck@gmail.com>
2024-01-09 19:41:03 -0500

    added 404 page and homepage routes

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
+// DEPENDENCIES
 const express = require('express')
+
+// CONFIG
+require('dotenv').config()
+const PORT = process.env.PORT
 const app = express()
 
+// HOMEPAGE
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hey, welcome to the party!')
 })
 
-app.listen(2424)
+// 404 PAGE
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
+})
+
+app.listen(PORT, () => {
+    console.log('this server is lit on', PORT)
+})
